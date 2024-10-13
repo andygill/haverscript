9bc049316bb0c8322c50b58676a1cbac51da1525
Andy Gill <andygillku@gmail.com>
2024-10-12 22:01:17 -0500

    Adding host argument to connect
    As part of this commit, the Ollama client connection
    has been refactored.

---
+++ examples/virtual_host/main.py
@@ -0,0 +1,14 @@
+import re
+from haverscript import connect, Host, Configuration
+
+
+class MyHost(Host):
+    def name(self):
+        return "myhost"
+
+    def chat(self, configuration: Configuration, prompt: str, stream: bool):
+        return f"I reject your {len(prompt.split())} word prompt, and replace it with my own."
+
+
+session = connect(host=MyHost()).echo()
+session = session.chat("In one sentence, why is the sky blue?")
