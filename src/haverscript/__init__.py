7c5c1d238c00c22cc67f7c39bfc12c7721b65b8b
Andy Gill <andygillku@gmail.com>
2024-11-02 15:08:39 -0500

    Adding list_models function

--- src/haverscript/__init__.py
+++ src/haverscript/__init__.py
@@ -8,6 +8,7 @@ from .haverscript import (
     connect,
     fresh,
     valid_json,
+    list_models,
 )
 
 __all__ = [
@@ -20,4 +21,5 @@ __all__ = [
     "connect",
     "fresh",
     "valid_json",
+    "list_models",
 ]
