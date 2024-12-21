655faf85d78799e4e1d3da19a0cb47f7108239dd
Andy Gill <andygillku@gmail.com>
2024-12-21 15:00:53 -0600

    use together python API

--- examples/together/main.py
+++ examples/together/main.py
@@ -1,12 +1,10 @@
-from haverscript import connect
-from haverscript.together import Together
+from haverscript.together import connect
+from haverscript import retry, echo
 from tenacity import stop_after_attempt, wait_fixed
 
 model = "meta-llama/Meta-Llama-3-8B-Instruct-Lite"
 session = (
-    connect(model, service=Together())
-    .retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
-    .echo()
+    connect(model) | retry(stop=stop_after_attempt(5), wait=wait_fixed(2)) | echo()
 )
 session = session.chat("Write a short sentence on the history of Scotland.")
 session = session.chat("Write 100 words on the history of Scotland.")
--- src/haverscript/together.py
+++ src/haverscript/together.py
@@ -1,11 +1,15 @@
 import json
 import logging
 import os
+from types import GeneratorType
+
 from dataclasses import dataclass
 
+import together
 import requests
 
-from .haverscript import Metrics
+
+from .haverscript import Metrics, Model, Service, model
 from .languagemodel import ServiceProvider, Reply, Request
 from .exceptions import LLMRateLimitError
 
@@ -18,122 +22,78 @@ class TogetherMetrics(Metrics):
 
 
 class Together(ServiceProvider):
+    client: together.Together | None = None
+    hostname = ""
+
     def __init__(self) -> None:
-        self.key = os.getenv("TOGETHER_API_KEY")
-        assert self.key is not None, "TOGETHER_API_KEY is not set to key"
+        key = os.getenv("TOGETHER_API_KEY")
+        assert key is not None, "TOGETHER_API_KEY is not set to key"
+        if self.client is None:
+            self.client: Together = together.Together(api_key=key)
 
-    def name(self):
-        return f"together.xyz"
+    def list(self) -> list[str]:
+        models = self.client.models.list()
+        return [model.id for model in models]
 
-    def list(self):
-        url = "https://api.together.xyz/v1/models"
-        headers = {"accept": "application/json", "authorization": f"Bearer {self.key}"}
-        response = requests.get(url, headers=headers)
-        models = response.json()
-        return [model["id"] for model in models]
+    def _suggestions(self, e: Exception):
+        # Slighty better message. Should really have a type of reply for failure.
+        if "ConnectError" in str(type(e)):
+            print("Connection error with together.ai")
+        return e
 
-    def _streaming(self, response: requests.Response):
+    def metrics(self, chunk: dict) -> Metrics:
+        return TogetherMetrics(
+            **{k: chunk[k] for k in TogetherMetrics.__dataclass_fields__.keys()}
+        )
 
-        buffer = ""
+    def generator(self, response):
 
-        def process_packet(packet):
-            if packet.startswith("data: "):
-                json_str = packet[len("data: ") :]
-                if json_str == "[DONE]":
-                    return None
-                try:
-                    json_data = json.loads(json_str)
-                    return json_data
-                except json.JSONDecodeError as e:
-                    raise ValueError("JSON decoding failed: {e}, <<{json_str}>>")
-            else:
-                packet_data = json.loads(packet)
-                if (
-                    "error" in packet_data
-                    and "type" in packet_data["error"]
-                    and packet_data["error"]["type"] == "credit_limit"
-                ):
-                    raise LLMRateLimitError
-
-        for chunk in response.iter_content(chunk_size=None, decode_unicode=True):
-            buffer += chunk
-            while "\n\n" in buffer:
-                packet, buffer = buffer.split("\n\n", 1)
-                json_data = process_packet(packet.strip())
-                if json_data is not None:
-                    if "usage" in json_data and isinstance(json_data["usage"], dict):
-                        yield TogetherMetrics(
-                            **{
-                                k: json_data["usage"][k]
-                                for k in TogetherMetrics.__dataclass_fields__.keys()
-                            }
-                        )
-
-                    yield json_data["choices"][0]["text"]
-
-        # Handle any remaining data in the buffer after the loop ends
-        if buffer.strip():
+        if isinstance(response, GeneratorType):
             try:
-                json_data = process_packet(buffer.strip())
-                if json_data is not None:
-                    yield json_data["choices"][0]["text"]
-            except ValueError:
-                pass
+                for chunk in response:
+                    if chunk.choices[0].finish_reason:
+                        yield self.metrics(chunk.usage.dict())
+                    yield from chunk.choices[0].delta.content
+            except Exception as e:
+                raise self._suggestions(e)
+        else:
+            assert isinstance(response.choices[0].message.content, str)
+            yield response.choices[0].message.content
+            yield self.metrics(response.usage.dict())
 
     def ask(self, request: Request):
+
         messages = []
-
-        prompt = request.prompt
-        model = request.contexture.model
-
-        kwargs = {}
-        kwargs["options"] = request.contexture.options
-        kwargs["context"] = request.contexture.context
-        kwargs["images"] = request.images
-        kwargs["stream"] = request.stream
 
         if request.contexture.system:
             messages.append({"role": "system", "content": request.contexture.system})
 
         for exchange in request.contexture.context:
-            pmt, imgs, resp = exchange.prompt, exchange.images, exchange.reply
-            assert imgs == (), f"imgs={imgs}"
-            messages.append({"role": "user", "content": pmt})
-            messages.append({"role": "assistant", "content": resp})
+            assert not exchange.images, "images not (yet) supported"
+            messages.append({"role": "user", "content": exchange.prompt})
+            messages.append({"role": "assistant", "content": exchange.reply})
 
-        messages.append({"role": "user", "content": prompt})
+        assert not request.images, "images not (yet) supported"
+        messages.append({"role": "user", "content": request.prompt})
 
-        url = "https://api.together.xyz/v1/chat/completions"
+        try:
+            assert isinstance(self.client, together.Together)
+            response = self.client.chat.completions.create(
+                model=request.contexture.model,
+                stream=request.stream,
+                messages=messages,
+                options=request.contexture.options,
+                format=request.format,
+            )
 
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "authorization": f"Bearer {self.key}",
-        }
+            return Reply(self.generator(response))
 
-        payload = {
-            "model": model,
-            "messages": messages,
-            "stream": request.stream,
-        } | request.contexture.options.copy()
+        except Exception as e:
+            raise self._suggestions(e)
 
-        response = requests.post(
-            url, json=payload, headers=headers, stream=request.stream
-        )
 
-        if request.stream:
-            return Reply(self._streaming(response))
-        else:
-            reply = response.json()
-            choices = reply["choices"]
-            assert len(choices) == 1
-            choice = choices[0]
-            metrics = None
-            if "usage" in reply and isinstance(reply["usage"], dict):
-                metrics = TogetherMetrics(
-                    **{
-                        k: reply["usage"][k]
-                        for k in TogetherMetrics.__dataclass_fields__.keys()
-                    }
-                )
-            return Reply([choice["message"]["content"], metrics])
+def connect(model_name: str | None = None) -> Model | Service:
+    service = Service(Together())
+    if model_name:
+        service = service | model(model_name)
+    return service
