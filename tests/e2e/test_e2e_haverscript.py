45af72d6739891983182c6d92b37ce8fea706704
Andy Gill <andygillku@gmail.com>
2024-12-23 09:55:30 -0600

    Fix together e2e examples

--- tests/e2e/test_e2e_haverscript.py
+++ tests/e2e/test_e2e_haverscript.py
@@ -3,20 +3,24 @@
 import subprocess
 import sys
 import os
+from pydantic import BaseModel, ConfigDict, Field
 
 import pytest
 import haverscript
+from haverscript.together import connect
 
 from tests.test_utils import remove_spinner
 
 
-def run_example(example, tmp_path, changes, args=[]):
+def run_example(example, tmp_path, header, changes, args=[]) -> str:
     filename = tmp_path / os.path.basename(example)
 
     with open(example, "r", encoding="utf-8") as f:
         content = f.read()
 
     # Apply the diffs
+    if header:
+        content = header.strip() + "\n" + content
     for old_text, new_text in changes.items():
         content = content.replace(old_text, new_text)
 
@@ -31,9 +35,52 @@ def run_example(example, tmp_path, changes, args=[]):
     )
 
     # check everything ran okay
+    if result.stderr:
+        print(result.stderr.decode("utf-8"))
+
+    print(result.stdout.decode("utf-8"))
+
     assert not result.stderr, "{result.stderr}"
 
     return remove_spinner(result.stdout.decode("utf-8"))
+
+
+class Similarity(BaseModel):
+    reasons: str
+    similarity: float
+
+
+def check_together(f1, f2):
+    with open(f1) as f:
+        s1 = f.read()
+    with open(f2) as f:
+        s2 = f.read()
+
+    s1 = f'"""{s1}"""'
+    s2 = f'"""{s2}"""'
+
+    prompt = f"""
+Here are two articles inside triple quotes. 
+Consider if the articles cover similar topics and provide similar information,
+and are of similar length.
+Give you answer as using JSON, and only JSON. There are two fields:
+ * "similarity": int # a score between 0 and 1 of how similar the articles are
+ * "reasons": str # a one sentence summary
+
+---
+article 1 : {s1}
+---
+article 2 : {s2}
+"""
+
+    response = connect("meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo").chat(
+        prompt, format=Similarity.model_json_schema()
+    )
+
+    similarity = Similarity.model_validate(response.value)
+
+    if similarity.similarity < 0.5:
+        raise AssertionError()
 
 
 def test_first_example(tmp_path, file_regression):
@@ -41,9 +88,9 @@ def test_first_example(tmp_path, file_regression):
         run_example(
             "examples/first_example/main.py",
             tmp_path,
+            "import haverscript as hs",
             {
-                '("mistral")': '("mistral:v0.3") | haverscript.options(seed=12345)',
-                "from haverscript": "import haverscript\nfrom haverscript",
+                '("mistral")': '("mistral:v0.3") | hs.options(seed=12345)',
             },
         ),
         extension=".txt",
@@ -51,29 +98,32 @@ def test_first_example(tmp_path, file_regression):
 
 
 def test_first_example_together(tmp_path, file_regression):
-    lines = len(
+    file_regression.check(
         run_example(
             "examples/first_example/main.py",
             tmp_path,
+            "import haverscript as hs\nimport haverscript.together as together",
             {
-                '("mistral")': '("meta-llama/Meta-Llama-3-8B-Instruct-Lite") | hs.retry(stop=hs.stop_after_attempt(5), wait=hs.wait_fixed(2))',
-                "from haverscript": "import haverscript as hs\nimport haverscript.together as together\nfrom haverscript",
+                '("mistral")': '("meta-llama/Meta-Llama-3-8B-Instruct-Lite") | hs.retry(stop=hs.stop_after_attempt(5), wait=hs.wait_fixed(2)) | hs.options(seed=12345)',
                 "connect(": "together.connect(",
             },
-        ).splitlines()
+        ),
+        extension=".txt",
+        check_fn=check_together,
     )
-    assert lines > 10 and lines < 1000
 
 
 def test_together(tmp_path, file_regression):
-    lines = len(
+    file_regression.check(
         run_example(
             "examples/together/main.py",
             tmp_path,
-            {},
-        ).splitlines()
+            "import haverscript as hs\n",
+            {"connect(model)": "connect(model) | hs.options(seed=12345)"},
+        ),
+        extension=".txt",
+        check_fn=check_together,
     )
-    assert lines > 10 and lines < 1000
 
 
 def test_tree_of_calls(tmp_path, file_regression):
@@ -81,7 +131,23 @@ def test_tree_of_calls(tmp_path, file_regression):
         run_example(
             "examples/tree_of_calls/main.py",
             tmp_path,
+            "",
             {'("mistral")': '("mistral:v0.3").options(seed=23456)'},
+        ),
+        extension=".txt",
+    )
+
+
+def test_tree_of_calls_together(tmp_path, file_regression):
+    file_regression.check(
+        run_example(
+            "examples/tree_of_calls/main.py",
+            tmp_path,
+            "import haverscript as hs\nimport haverscript.together as together",
+            {
+                '("mistral")': '("meta-llama/Meta-Llama-3-8B-Instruct-Lite") | hs.options(seed=12345)',
+                "connect(": "together.connect(",
+            },
         ),
         extension=".txt",
     )
@@ -92,6 +158,7 @@ def test_chaining_answers(tmp_path, file_regression):
         run_example(
             "examples/chaining_answers/main.py",
             tmp_path,
+            "",
             {'("mistral")': '("mistral:v0.3").options(seed=12345)'},
         ),
         extension=".txt",
@@ -103,6 +170,7 @@ def test_options(tmp_path, file_regression):
         run_example(
             "examples/options/main.py",
             tmp_path,
+            "",
             {"(seed=None)": "(seed=56789)"},
         ),
         extension=".txt",
@@ -118,6 +186,7 @@ def test_cache(tmp_path, file_regression):
         run_example(
             "examples/cache/main.py",
             tmp_path,
+            "",
             env,
             args=["2"],
         ),
@@ -127,6 +196,7 @@ def test_cache(tmp_path, file_regression):
         run_example(
             "examples/cache/main.py",
             tmp_path,
+            "",
             env,
             args=["3"],
         ),
@@ -139,6 +209,7 @@ def test_validate(tmp_path, file_regression):
         run_example(
             "examples/validate/main.py",
             tmp_path,
+            "",
             {
                 '("mistral")': '("mistral:v0.3")',
                 'blue?")': 'blue?", middleware=haverscript.options(seed=12345))',
@@ -156,6 +227,7 @@ def test_images(tmp_path, file_regression):
         run_example(
             "examples/images/main.py",
             tmp_path,
+            "",
             {'("llava")': '("llava:v1.6").options(seed=12345)'},
         ),
         extension=".txt",
@@ -174,7 +246,8 @@ def test_meta(tmp_path, file_regression):
         run_example(
             "examples/meta_model/main.py",
             tmp_path,
-            {'("mistral")': '("mistral:v0.3") | options(seed=12345)'},
+            "import haverscript as hs",
+            {'("mistral")': '("mistral:v0.3") | hs.options(seed=12345)'},
         ),
         extension=".txt",
     )
