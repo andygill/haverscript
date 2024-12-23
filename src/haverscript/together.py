de40909dbe52a7658093b131546686abc02e42c3
Andy Gill <andygillku@gmail.com>
2024-12-23 09:55:30 -0600

    Fix together e2e examples

--- src/haverscript/together.py
+++ src/haverscript/together.py
@@ -25,11 +25,12 @@ class Together(ServiceProvider):
     client: together.Together | None = None
     hostname = ""
 
-    def __init__(self) -> None:
-        key = os.getenv("TOGETHER_API_KEY")
-        assert key is not None, "TOGETHER_API_KEY is not set to key"
+    def __init__(self, api_key: str | None = None) -> None:
+        if api_key is None:
+            api_key = os.getenv("TOGETHER_API_KEY")
+        assert api_key is not None, "need TOGETHER_API_KEY"
         if self.client is None:
-            self.client: Together = together.Together(api_key=key)
+            self.client: Together = together.Together(api_key=api_key)
 
     def list(self) -> list[str]:
         models = self.client.models.list()
@@ -53,14 +54,14 @@ class Together(ServiceProvider):
                 for chunk in response:
                     for choice in chunk.choices:
                         if choice.finish_reason and chunk.usage:
-                            yield self.metrics(chunk.usage.dict())
+                            yield self.metrics(chunk.usage.model_dump())
                         yield choice.delta.content
             except Exception as e:
                 raise self._suggestions(e)
         else:
             assert isinstance(response.choices[0].message.content, str)
             yield response.choices[0].message.content
-            yield self.metrics(response.usage.dict())
+            yield self.metrics(response.usage.model_dump())
 
     def ask(self, request: Request):
 
@@ -93,10 +94,9 @@ class Together(ServiceProvider):
                 "logprobs",
             ]
         )
-
         kwargs = {
-            key: value
-            for key, value in request.contexture.options
+            key: request.contexture.options[key]
+            for key in request.contexture.options
             if key in together_keywords
         }
 
@@ -122,8 +122,12 @@ class Together(ServiceProvider):
             raise self._suggestions(e)
 
 
-def connect(model_name: str | None = None) -> Model | Service:
-    service = Service(Together())
+def connect(
+    model_name: str | None = None, api_key: str | None = None
+) -> Model | Service:
+    """return a model or service that uses the given model name."""
+
+    service = Service(Together(api_key=api_key))
     if model_name:
         service = service | model(model_name)
     return service
