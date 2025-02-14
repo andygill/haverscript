89a28b7578bb970c7673714e4954a79f62dbbb23
Andy Gill <andygillku@gmail.com>
2025-02-13 23:06:04 -0600

    refactor meta model
    we now use an agent

--- examples/meta_model/main.py
+++ examples/meta_model/main.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Iterator
 
 from haverscript import *
 from haverscript.types import *
@@ -6,60 +7,64 @@ from haverscript.middleware import *
 
 
 class Translate(BaseModel):
-    english: str
-    french: str
+    english: str = Field(
+        ..., description="the original English text, and only the original text."
+    )
+    french: str = Field(
+        ..., description="the translated French text, and only the translated text."
+    )
 
 
-class ExampleMetaModel(MetaModel):
-    previous: list = Field(default_factory=list)
+class MetaAgent(Agent, ServiceProvider):
+    system: str = "You are a translator, translating from English to French. "
+    previous: list[Translate] = Field(default_factory=list)
 
-    def chat(self, prompt, next: LanguageModel) -> Reply:
+    def list(self) -> list[str]:
+        return ["french"]
 
+    def ask(self, request: Request) -> Reply:
+        assert isinstance(request, Request)
+        assert isinstance(request.prompt, Prompt)
+        assert isinstance(request.contexture, Contexture)
+
+        return Reply(self._stream(request))
+
+    def _stream(self, request) -> Iterator:
+        max_traslations = 3
         if len(self.previous) >= 3:
-            response = Reply("Sorry. French lesson over.\nYour 3 translations:\n")
+            yield f"Sorry. French lesson over.\nYour {max_traslations} translations:\n"
             for resp in self.previous:
                 assert isinstance(resp, Translate)
-                response += Reply(f"* {resp.english} => {resp.french}\n")
+                yield f"* {resp.english} => {resp.french}\n"
+        else:
+            prompt = Markdown()
+            prompt += f"You are a translator, translating from English to French. "
+            prompt += f"English Text: {request.prompt.content}"
+            prompt += reply_in_json(Translate)
 
-            return response
+            translated: Translate = self.ask_llm(prompt, format=Translate)
 
-        next = next | format(Translate)
+            self.previous.append(translated)
+            remaining = max_traslations - len(self.previous)
 
-        request = Request(
-            contexture=Contexture(model="mistral"),
-            prompt=Prompt(
-                content=f"You are a translator, translating from English to French. "
-                f'Give your answer as a JSON record with two fields, "english", and "french". '
-                f'The "english" field should contain the original English text, and only the original text.'
-                f'The "french" field should contain the translated French text, and only the translated text.'
-                f"\n\nEnglish Text: {prompt}"
-            ),
-        )
-        response = next.ask(request)
-        translated: Translate = response.value
-
-        self.previous.append(translated)
-        remaining = 3 - len(self.previous)
-
-        return Reply(
-            f"{translated.english} in French is {translated.french}\n{remaining} translation(s) left."
-        )
+            yield f"{translated.english} in French is {translated.french}\n"
+            yield "\n"
+            yield f"{remaining} translation(s) left."
 
 
 # In a real example, validate and retry would be added to provide robustness.
-model = connect("mistral") | meta(ExampleMetaModel) | echo()
-
+session = Service(MetaAgent(model=connect("mistral"))) | model("french") | echo()
 
 print("--[ User-facing conversation ]------")
-session = model.chat("Three blind mice")
+session = session.chat("Three blind mice")
 session = session.chat("Such is life")
 session = session.chat("All roads lead to Rome")
 session = session.chat("The quick brown fox")
 
-model = connect("mistral") | echo() | meta(ExampleMetaModel)
+session = Service(MetaAgent(model=connect("mistral") | echo())) | model("french")
 
 print("--[ LLM-facing conversation ]------")
-session = model.chat("Three blind mice")
+session = session.chat("Three blind mice")
 session = session.chat("Such is life")
 session = session.chat("All roads lead to Rome")
 session = session.chat("The quick brown fox")
