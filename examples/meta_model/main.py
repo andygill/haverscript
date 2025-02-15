72a7070d60ff8582eb811be2ca4e1ca93f738860
Andy Gill <andygillku@gmail.com>
2025-02-15 11:32:46 -0600

    Adding chatbot
    Typically, a chatbot will be an agent

--- examples/meta_model/main.py
+++ examples/meta_model/main.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Iterator
+from typing import Iterator, Protocol, Callable
 
 from haverscript import *
-from haverscript.types import *
-from haverscript.middleware import *
+from haverscript.types import Reply, Prompt, Contexture
+from copy import deepcopy
 
 
 class Translate(BaseModel):
@@ -15,34 +15,27 @@ class Translate(BaseModel):
     )
 
 
-class MetaAgent(Agent, ServiceProvider):
+class FrenchAgent(Agent):
     system: str = "You are a translator, translating from English to French. "
     previous: list[Translate] = Field(default_factory=list)
 
-    def list(self) -> list[str]:
-        return ["french"]
+    def chat(self, prompt: str) -> Reply:
+        return Reply(self._stream(prompt))
 
-    def ask(self, request: Request) -> Reply:
-        assert isinstance(request, Request)
-        assert isinstance(request.prompt, Prompt)
-        assert isinstance(request.contexture, Contexture)
-
-        return Reply(self._stream(request))
-
-    def _stream(self, request) -> Iterator:
+    def _stream(self, prompt) -> Iterator:
         max_traslations = 3
-        if len(self.previous) >= 3:
+        if len(self.previous) >= max_traslations:
             yield f"Sorry. French lesson over.\nYour {max_traslations} translations:\n"
             for resp in self.previous:
                 assert isinstance(resp, Translate)
                 yield f"* {resp.english} => {resp.french}\n"
         else:
-            prompt = Markdown()
-            prompt += f"You are a translator, translating from English to French. "
-            prompt += f"English Text: {request.prompt.content}"
-            prompt += reply_in_json(Translate)
+            down_prompt = Markdown()
+            down_prompt += f"You are a translator, translating from English to French. "
+            down_prompt += f"English Text: {prompt}"
+            down_prompt += reply_in_json(Translate)
 
-            translated: Translate = self.ask_llm(prompt, format=Translate)
+            translated: Translate = self.ask_llm(down_prompt, format=Translate)
 
             self.previous.append(translated)
             remaining = max_traslations - len(self.previous)
@@ -53,7 +46,8 @@ class MetaAgent(Agent, ServiceProvider):
 
 
 # In a real example, validate and retry would be added to provide robustness.
-session = Service(MetaAgent(model=connect("mistral"))) | model("french") | echo()
+
+session = connect_chatbot(FrenchAgent(model=connect("mistral"))) | echo()
 
 print("--[ User-facing conversation ]------")
 session = session.chat("Three blind mice")
@@ -61,7 +55,7 @@ session = session.chat("Such is life")
 session = session.chat("All roads lead to Rome")
 session = session.chat("The quick brown fox")
 
-session = Service(MetaAgent(model=connect("mistral") | echo())) | model("french")
+session = connect_chatbot(FrenchAgent(model=connect("mistral") | echo()))
 
 print("--[ LLM-facing conversation ]------")
 session = session.chat("Three blind mice")
